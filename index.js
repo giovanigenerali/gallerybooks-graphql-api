11c2351103af84104d77b075672ddb69ee6bdf50
Giovani Generali <giovani.generali@gmail.com>
2020-09-19 15:43:14 -0300

    update console server listen

--- index.js
+++ index.js
@@ -117,5 +117,5 @@ const server = new ApolloServer({
 const port = process.env.PORT || 5000;
 
 server.listen(port).then(({ url }) => {
-  console.log(`Server is running at ${url}`);
+  console.log(`🚀 Server running at ${url}`);
 });
